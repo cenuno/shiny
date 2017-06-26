b9169cd6f939da86af8b971a83120602fc33e98a
Cristian E. Nuno <nuno.e.cristian@gmail.com>
2017-06-26 16:29:45 -0400

    Eliminated Comment

--- DT-Download-All-Rows-Button/app.r
+++ DT-Download-All-Rows-Button/app.r
@@ -1,6 +1,5 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# This is a Shiny web application.
 #
 # Find out more about building applications with Shiny here:
 #
