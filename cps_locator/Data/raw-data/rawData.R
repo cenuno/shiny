3fd7c069532c4375e9fc71b5b453dacf1d409760
Cristian E. Nuno <nuno.e.cristian@gmail.com>
2017-09-09 15:34:54 -0500

    Added saveRDS commands to rawData.R

--- cps_locator/Data/raw-data/rawData.R
+++ cps_locator/Data/raw-data/rawData.R
@@ -20,6 +20,11 @@ comarea606 <- rgdal::readOGR( dsn = geojson_comarea_url
                        , stringsAsFactors = FALSE
 )
 
+# export comarea606 as RDS in the /Data/raw-data folder
+saveRDS( object = comarea606
+         , file = "/Users/cristiannuno/RStudio_All/shiny/cps_locator/Data/raw-data/comarea606_raw.RDS"
+         )
+
 # import chicago public school 
 # information data for the 2016-2017 school year (SY1617)
 cps_sy1617_url <- "https://data.cityofchicago.org/api/views/8i6r-et8s/rows.csv?accessType=DOWNLOAD"
@@ -30,6 +35,12 @@ cps_sy1617 <- read.csv( file = cps_sy1617_url
                         , stringsAsFactors = FALSE
 )
 
+# export cps_sy1617 as RDS in the /Data/raw-data folder
+saveRDS( object = cps_sy1617
+         , file = "/Users/cristiannuno/Rstudio_All/shiny/cps_locator/Data/raw-data/cps_sy1617_raw.RDS"
+         )
+
 #########################################################
 ### Successfully Imported Raw Data into R Environment ###
+### and Exported as .RDS files                        ###
 #########################################################
