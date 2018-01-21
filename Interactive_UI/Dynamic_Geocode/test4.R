cbde77748488522561e23b4c373499419815e422
cenuno <nuno.e.cristian@gmail.com>
2018-01-21 15:29:20 -0600

    Dynamic color and size change is accomplished by hovering cursor over a circleMarker.

--- Interactive_UI/Dynamic_Geocode/test4.R
+++ Interactive_UI/Dynamic_Geocode/test4.R
@@ -1,26 +1,18 @@
 #
 # Author:     Cristian E. Nuno
 # Date:       January 20, 2018
-# Purpose:    Change color of Cicle Marker as mouse hovers over them
+# Purpose:    Change color and size of Cicle Marker as mouse hovers over it
 #
 
 # load necessary packages
 library( leaflet )
 library( shiny )
-library( sp )
-library( rgdal )
 
-# load necessary data
-cps.school.location.sy1718 <-
-  rgdal::readOGR( dsn = "https://data.cityofchicago.org/api/geospatial/4g38-vs8v?method=export&format=GeoJSON"
-                  , layer = "OGRGeoJSON"
-                  , stringsAsFactors = FALSE
-                  )
 
 ## make UI ##
 ui <- shiny::fluidPage(
-  title = "Chicago Public Schools - School Locations SY1718"
-  , leaflet::leafletOutput( outputId = "map"
+  title = "Test"
+  , leaflet::leafletOutput( outputId = "myMap"
                             , height = 900
   )
   , theme = "dynamic_colors.css"
@@ -30,24 +22,25 @@ ui <- shiny::fluidPage(
 server <- function( input, output){
   
   # render the map
-  output$map <- leaflet::renderLeaflet({
-    leaflet() %>%
+  output$myMap <- leaflet::renderLeaflet({
+    leaflet( options = leafletOptions( minZoom = 11 ) ) %>%
       addTiles( urlTemplate = "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png") %>%
-      setView( lng = -87.567215
-               , lat = 41.822582
+      setView( lng = -87.655333
+               , lat = 41.948438
                , zoom = 11 ) %>%
-      addCircleMarkers( lng = as.numeric( cps.school.location.sy1718$long )
-                        , lat = as.numeric( cps.school.location.sy1718$lat )
-                        , color = "#10539A"
-                        , opacity = 0.5
-                        , fillOpacity = 0.5
+      addCircleMarkers( lng = c( -87.655333,  -87.633752)
+                        , lat = c( 41.948438, 41.829902 )
+                        , fillColor = "#10539A"
+                        , fillOpacity = 0.75
                         , stroke = FALSE
-                        , radius = 10
+                        , radius = 10 # units in meters for circles
       ) 
   })
+  
   
 } # end of server
 
 # run Shiny app
 shinyApp( ui = ui, server = server )
 
+# end of script #
